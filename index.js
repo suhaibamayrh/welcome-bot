1ec7dc3ded31d62bb9f5483a926f4cbbafbcaf52
suhaibamayrh <101183580+suhaibamayrh@users.noreply.github.com>
2022-03-08 14:07:24 +0300

    .env
    OTUwNjk5NjU2NjM4MjQyODU2.YicuDQ.pb3Lc_PJ3Y0Sy6SixBRqd5e0Hck

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 const db = require('quick.db')
 
-const prefix = "+"
+const prefix = "!"
 
 client.on('ready', async () => {
   console.clear()
